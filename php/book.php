c22be662916d0e6490291720fe495dcc9f0d0859
Shruthisdst <shruthitr.nayak@gmail.com>
2015-07-09 18:27:49 +0530

    Commit No. 1 - Initial commit

---
+++ php/book.php
@@ -0,0 +1,17 @@
+<?php include("include_header.php");?>
+<main class="cd-main-content">
+		<div class="cd-scrolling-bg cd-color-2">
+			<div class="cd-container">
+			<h1 class="clr1 gapBelowSmall">Books List</h1>
+			<div class="article">
+				<div class="gapBelowSmall">
+					<span class="aFeature clr2">page range</span><br />
+					<span class="aTitle">Book Title</span><br />
+					<span class="aAuthor itl">Authorname</span>
+				</div>
+			</div>
+
+			</div> <!-- cd-container -->
+		</div> <!-- cd-scrolling-bg -->
+	</main> <!-- cd-main-content -->
+<?php include("include_footer.php");?>
